09f305ed47e91e3c4eec00dfc5e427f98dd681d9
Rajat Gupta <rajat.gupta@veeam.com>
2022-11-26 00:55:41 +0530

    Config Done
    Signed-off-by: Rajat Gupta <rajat.gupta@veeam.com>

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = "sports-management"
+app.config['MONGO_URI'] = "mongodb+srv://vishal:hP7Wa8HV65pzSs4y@sports-management.ihvhhwb.mongodb.net/?retryWrites" \
+                          "=true&w=majority "
 
 
 @app.after_request
@@ -12,3 +17,6 @@ def add_headers(response):
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     response.headers.add('Content-Type', 'application/json')
     return response
+
+
+mongo = PyMongo(app)
