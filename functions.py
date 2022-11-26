d4a63855bc617a128ff1db1a0e6d3037fecbb477
Rajat Gupta <rajat.gupta@veeam.com>
2022-11-26 17:03:32 +0530

    Login Register Done
    Signed-off-by: Rajat Gupta <rajat.gupta@veeam.com>

--- functions.py
+++ functions.py
@@ -5,10 +5,14 @@ from config import mongo
 def login(username, password):
     db = mongo['sports-management']
     coll = db['users']
-    data = coll.find_one({
-        constant.USER_NAME: username
-    })
-    if len(data) != 0:
+    try:
+        data = coll.find_one({
+            constant.USER_NAME: username
+        })
+    except Exception as e:
+        return Exception(e)
+
+    if data is not None:
         if password == data[constant.PASSWORD]:
             return True
         else:
@@ -17,13 +21,23 @@ def login(username, password):
         return False
 
 
-def register(username, password, email, gender, age, sport):
+def register(full_name, username, password, email, gender, age, sport):
     db = mongo['sports-management']
     coll = db['users']
     try:
+        try:
+            data = coll.find_one({
+                constant.USER_NAME: username
+            })
+        except Exception as e:
+            return Exception(e)
+        if data is not None:
+            return "User with Username " + username + " Already Exists"
+
         data = coll.insert_one({
             constant.USER_NAME: username,
             constant.PASSWORD: password,
+            constant.FULL_NAME: full_name,
             constant.EMAIL: email,
             constant.GENDER: gender,
             constant.AGE: age,
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 import functions
 
 
@@ -13,25 +13,34 @@ def login():
     username = request.form['username']
     password = request.form['password']
     status = functions.login(username, password)
-    if status:
-        return "Login Successfully"
+    if status is True:
+        message = "Login Successfully"
+        return render_template("index.html", message=message)
     else:
-        return "Login Failed"
+        error = "Login Failed"
+        return render_template("index.html", error=error)
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register')
 def register():
+    return render_template('register.html')
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
     username = request.form['username']
     password = request.form['password']
+    full_name = request.form['full_name']
     email = request.form['email']
     gender = request.form['gender']
     age = request.form['age']
     sport = request.form['sport']
-    status = functions.register(username, password, email, gender, age, sport)
-    if status:
-        return render_template('index.html')
+    status = functions.register(full_name, username, password, email, gender, age, sport)
+    if status is True:
+        render_template('register.html', message="User Registered")
+        return redirect('home')
     else:
-        return render_template('index.html')
+        return render_template('register.html', error=status)
 
 
 if __name__ == '__main__':
